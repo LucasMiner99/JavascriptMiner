94228b6fb04982bfbb62b642cc9b1d859273284f
Lucas Miner <lucasminer99@gmail.com>
2021-09-17 21:09:29 -0300

    primer commit configurando git

---
+++ clase1.js
@@ -0,0 +1,61 @@
+// Comentario simple
+/* Comentario
+Multilinea */
+
+// Variables
+
+
+var numeroUno = 30;
+
+var nombreUsuario;
+
+var apellidoUsuario;
+
+numeroUno = 100;
+
+var mensaje = "Mensaje enviado desde la variable Mensaje"
+
+alert ("ATENCION");
+nombreUsuario = prompt("Ingrese su nombre");
+apellidoUsuario = prompt("Ingrese su apellido");
+
+
+
+
+console.log("Bienvenido al sistema " + nombreUsuario + " " + apellidoUsuario);
+
+
+
+var numeroA = 20;
+var numeroB = 40;
+
+
+var numeroC;
+
+
+// SUMA //
+
+numeroC = 30 + 30;
+
+// RESTA //
+
+numeroC = 100 - 20;
+
+// DIVISION //
+
+numeroC = 100 / 2;
+
+// MULTIPLICACION //
+
+numeroC = 100 * 2;
+
+// CONCATENACION // 
+
+numeroC = "string" + 20;
+
+var numeroUno = prompt("Ingrese el primer numero");
+var numeroDos = prompt("Ingrese el segundo numero");
+
+var resultado = parseInt(numeroUno) + parseInt(numeroDos);
+
+alert ("Bueno " + nombreUsuario + " " + apellidoUsuario + " el resultado es " + resultado);
