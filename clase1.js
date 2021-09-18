ea114418570b1edbae0c4e7194a4a4d912f2035f
Lucas Miner <lucasminer99@gmail.com>
2021-09-17 21:39:02 -0300

    nose que paso pero mando otro

--- clase1.js
+++ clase1.js
@@ -1,4 +1,4 @@
-// Comentario simple
+  // Comentario simple
 /* Comentario
 Multilinea */
 
@@ -18,8 +18,6 @@ var mensaje = "Mensaje enviado desde la variable Mensaje"
 alert ("ATENCION");
 nombreUsuario = prompt("Ingrese su nombre");
 apellidoUsuario = prompt("Ingrese su apellido");
-
-
 
 
 console.log("Bienvenido al sistema " + nombreUsuario + " " + apellidoUsuario);
@@ -59,3 +57,29 @@ var numeroDos = prompt("Ingrese el segundo numero");
 var resultado = parseInt(numeroUno) + parseInt(numeroDos);
 
 alert ("Bueno " + nombreUsuario + " " + apellidoUsuario + " el resultado es " + resultado);
+
+alert ("Ahora calculemos el area de un cuadrado");
+
+var numCuadrado1 = prompt("Ingrese la medida de un lado del cuadrado");
+
+var resultadoArea = parseInt(numCuadrado1) * parseInt(numCuadrado1);
+
+alert ("El area del cuadrado es de " + resultadoArea);
+
+var numeroAleatorio = prompt("Pone un numero y te digo el que sigue");
+
+var numeroConsecutivo =  parseInt(numeroAleatorio) + 1;
+
+alert ("El numero que le sigue es " + numeroConsecutivo);
+
+var edadA = prompt("Decime tu edad");
+
+var edadB = prompt("Ahora la de tu abuelo");
+
+var diferenciaEdad = parseInt(edadB) - parseInt(edadA);
+
+alert ("Vos y tu abuelo se llevan " + diferenciaEdad + " años.");
+
+var edadNacimiento = 2021 - prompt("En que ano naciste?") ;
+
+alert ("Tenes " + edadNacimiento + " años.")
